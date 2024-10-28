10701a8702971c784f7a483d2654076767f9668e
Saif Anwar <anwarsaif99@hotmail.com>
2024-10-28 19:37:48 +0000

    update

--- assign_tasks.py
+++ assign_tasks.py
@@ -1,6 +1,14 @@
 from first_test import factor, arrival, bonus, reward, duration, time_bonus
 from trivial_sample_solution import assign_tasks
 
+#factor = factor[:10]
+#arrival = arrival[:10]
+#bonus = bonus[:10]
+#reward = reward[:10]
+#duration = duration[:10]
+#time_bonus = time_bonus[:10]
+
+
 def sample_assign_tasks(factor, arrival, bonus, reward, duration, time_bonus):
     """Assign tasks to processors."""
     """Simple dumb strategy, guaranteed to produce valid solutions."""
@@ -17,6 +25,7 @@ def sample_assign_tasks(factor, arrival, bonus, reward, duration, time_bonus):
 def assign_tasks(factor, arrival, bonus, reward, duration, time_bonus):
     import numpy as np
     import random
+    import math
 
     num_tasks = len(factor)
     max_arrival = max(arrival)
@@ -83,10 +92,8 @@ def assign_tasks(factor, arrival, bonus, reward, duration, time_bonus):
             for i in range(num_tasks):
                 if arrival[i] <= current_time:
                     if i not in tasks:
-                        print(i, current_time)
                         possible_actions.append((i, current_time))
             current_time += 1
-        print(possible_actions)
         return possible_actions
 
     class TreeNode:
@@ -96,6 +103,7 @@ def assign_tasks(factor, arrival, bonus, reward, duration, time_bonus):
             self.children = []
             self.value = 0
             self.visits = 0
+            self.expanded = False
 
         def best_child(self, exploration_weight=1.4):
                 choices_weights = [
@@ -107,15 +115,19 @@ def assign_tasks(factor, arrival, bonus, reward, duration, time_bonus):
                 return self.children[np.argmax(choices_weights)]
 
         def is_fully_expanded(self, ):
-            num_tasks_scheduled = len([x for x in self.state if x[0] is not None])
-            return num_tasks == num_tasks_scheduled
+            return self.expanded
 
         def expand(self):
             actions = get_possible_actions(self.state)
+            print(actions)
             for action in actions:
-                new_state = take_action(action, self.state)
+                new_state = self.state.copy()
+                new_state[action[0]] = (0, action[1])
+#                print(state)
+#                new_state = take_action(action, self.state)
                 child_node = TreeNode(new_state, parent=self)
                 self.children.append(child_node)
+            self.expanded = True
 
         def update(self, score):
             self.visits += 1
@@ -127,6 +139,7 @@ def assign_tasks(factor, arrival, bonus, reward, duration, time_bonus):
             self.root = root
 
         def select(self, node):
+#            print('selection', is_terminal(node.state), node.is_fully_expanded())
             while not is_terminal(node.state) and node.is_fully_expanded():
                 node = node.best_child()
             return node
@@ -137,17 +150,22 @@ def assign_tasks(factor, arrival, bonus, reward, duration, time_bonus):
             return random.choice(node.children)
 
         def simulate(self, node):
+            counter = 0
             valid_schedule = False
-            while not valid_schedule:
+            while not valid_schedule and counter < 1000:
                 current_state = node.state.copy()
                 while not is_terminal(current_state):
                     actions = get_possible_actions(current_state)
-                    print(actions)
                     action = random.choice(actions)
+#                    print(action, duration[action[0]])
                     current_state = take_action(action, current_state)
                 valid_schedule = is_valid_schedule(current_state)
                 simulated_score = self.schedule_score_func(current_state)
+                counter += 1
 #            print(current_state)
+#            if counter == 1000:
+#                return 0
+#            else:
             return simulated_score
 
         def backpropagate(self, node, score):
@@ -157,13 +175,17 @@ def assign_tasks(factor, arrival, bonus, reward, duration, time_bonus):
 
         def run(self, num_simulations):
             for _ in range(num_simulations):
+#                print(self.root.state)
                 node = self.select(self.root)
+                print(is_terminal(node.state))
                 if not is_terminal(node.state):
                     node = self.expand(node)
                 score = self.simulate(node)
+                print(score)
                 self.backpropagate(node, score)
 
         def best_action(self):
+            print([child.state for child in self.root.children])
             return max(self.root.children, key=lambda child: child.visits)
 
         def schedule_score_func(self, schedule):
@@ -184,7 +206,7 @@ def assign_tasks(factor, arrival, bonus, reward, duration, time_bonus):
     # sort a list of tuples by the second element in each tuple and return the indexes
         task_indices = [i[0] for i in sorted(enumerate(schedule), key=lambda x:x[1][1])]
 #        print(task_indices)
-        print([( schedule[i], duration[i] ) for i in task_indices])
+#        print([( schedule[i], duration[i] ) for i in task_indices])
 
         for i in range(num_tasks):
             for j in range(i+1, num_tasks):
@@ -194,9 +216,16 @@ def assign_tasks(factor, arrival, bonus, reward, duration, time_bonus):
 
 
     root = TreeNode(schedule)
-    mcts = MCTS(root)
-    mcts.run(100000)
-    best_action = mcts.best_action()
+    node = root
+
+    while not is_terminal(node.state):
+        mcts = MCTS(node)
+        mcts.run(10)
+        best_action = mcts.best_action()
+        node = best_action
+        print(node.state)
+
+    print(best_action.state)
 
 
 
